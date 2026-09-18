058abbbc7904dcf411507731310b1b3d5f03380b
Ivan Pambudi <pambu.design@gmail.com>
2026-09-18 22:32:58 +0700

    indo as default

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ export const routing = defineRouting({
   locales: ["id", "en", "zh"],
   defaultLocale: "id",
   localePrefix: "as-needed",
+  localeDetection: false,
 });
 
 export type AppLocale = (typeof routing.locales)[number];
